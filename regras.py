41f8201562bdb068b6aa6926c2ef5ff14f9e81f4
thiagootuler <thiagootuler@hotmail.com>
2017-05-01 00:00:00 -0300

    Versao 03 das regras da arvore de decisao

--- regras.py
+++ regras.py
@@ -2,11 +2,253 @@
 """
     Identificacao: Alerta de risco de incendio
     Autor: Ronie Jovanhol
+           Thiago Tuler
     Utilidades: Dado os preditores de entrada, Esse algoritmo fornece um
-                percentual de risco de incendio por meio de um conjunto 
-                de regras gerados por uma arvore de decisao.
+                percentual de risco de incendio por meio da navegação na 
+                arvore CART.
 """
+# Make a prediction with a decision tree
+def predict(node, row):
+	if row[node['index']] <= node['value']:
+		if isinstance(node['left'], dict):
+			return predict(node['left'], row)
+		else:
+			return node['left']
+	else:
+		if isinstance(node['right'], dict):
+			return predict(node['right'], row)
+		else:
+			return node['right']
+
 if __name__ == '__main__':
+    dataset = [
+        #['dens_demo', 'renda', 'SLOPE', 'prec', 'VCF', 'uso_terra', 'DEM', 'temp']
+        [3.63463, 556.09497, 0, 1034.39551, 0, 8, 0, 0, 0.0835317],
+        [3.63463, 556.09497, 0, 1090.74597, 0, 8, 0, 0, 0.330305],
+        [3.63463, 556.09497, 0, 1090.74598, 0, 8, 0, 0, 0.18456],
+        [3.63463, 841.71002, 0, 1109.07446, 0, 8, 0, 0, 0.110923],
+        [3.63463, 864.86499, 0, 1109.07446, 0, 8, 0, 0, 0.434419],
+        [3.63463, 864.865, 0, 1109.07446, 0, 8, 0, 0, 0.175612],
+        [3.26368, 0, 0, 1109.07447, 54.5, 8, 0, 0, 0.109868],
+        [3.26368, 0, 0, 1109.07447, 54.6, 8, 44.50000, 24.79185, 0.0168376],
+        [3.26368, 0, 0, 1109.07447, 54.6, 8, 44.50000, 24.79186, 0.131192],
+        [3.26368, 0, 0, 1109.07447, 54.6, 8, 44.50001, 0, 0.0499697],
+        [3.26369, 0, 0, 1109.07447, 0, 8, 0, 0, 0.139411],
+        [3.26369, 859.72498, 0, 1350.50757, 0, 9, 0, 24.75305, 0.0372407],
+        [3.26369, 859.72498, 0, 1350.50757, 0, 9, 0, 24.75306, 0.150805],
+        [3.26369, 859.72499, 0, 1350.50757, 0, 9, 0, 0, 0.250526],
+        [3.26369, 684.70502, 0, 1350.50758, 0, 9, 0, 24.68275, 0.281141],
+        [3.26369, 684.70503, 0, 1350.50758, 0, 9, 0, 24.68275, 0.748276],
+        [2.35704, 859.72499, 0, 1350.50758, 0, 9, 0, 24.68275, 0.0606344],
+        [2.35705, 859.72499, 0, 1350.50758, 0, 9, 0, 24.68275, 0.52072],
+        [2.35704, 684.70503, 0, 1350.50758, 0, 9, 0, 24.71705, 0.0901171],
+        [2.35704, 684.70503, 0, 1350.50758, 0, 9, 0, 24.71706, 0.338749],
+        [2.50642, 684.70503, 0, 1350.50758, 0, 9, 0, 24.68276, 0.239003],
+        [2.50643, 684.70503, 0, 1350.50758, 0, 9, 0, 24.68276, 0.571113],
+        [3.63464, 0, 0, 1169.50452, 0, 0, 55.50000, 25.03165, 0.185039],
+        [3.63464, 0, 0, 1169.50452, 0, 0, 55.50000, 25.03166, 0.0699455],
+        [3.63464, 736.80499, 0, 1169.50453, 0, 6, 55.50000, 25.03165, 0.0639006],
+        [3.63464, 736.80499, 0, 1371.60449, 0, 7, 55.50000, 25.03165, 0.125505],
+        [24.23654, 736.80499, 0, 1371.6045, 0, 7, 55.50000, 25.03165, 0.0750805],
+        [24.23655, 736.80499, 0, 1371.6045, 0, 7, 55.50000, 25.03165, 0.586747],
+        [3.63464, 736.805, 0, 1169.50453, 0, 0, 55.50000, 25.03165, 0.0473021],
+        [6.08065, 506.88501, 0, 1169.50453, 0, 0, 55.50001, 0, 0.0315917],
+        [4.63185, 506.88502, 0, 1169.50453, 0, 0, 55.50001, 0, 0.0978987],
+        [4.63186, 506.88502, 0, 1169.50453, 0, 0, 55.50001, 0, 0.202195],
+        [5.8944, 624.57502, 0, 1169.50453, 0, 0, 55.50001, 0, 0.0575384],
+        [5.89441, 624.57502, 0, 1169.50453, 0, 0, 55.50001, 0, 0.175359],
+        [6.08066, 624.57502, 5.29967, 1130.84399, 0, 0, 55.50001, 0, 0.0818293],
+        [6.08066, 624.57502, 5.29968, 1130.84399, 0, 0, 55.50001, 0, 0.0408473],
+        [6.08066, 624.57502, 0, 1130.84400, 0, 0, 55.50001, 0, 0.0214471]
+    ]
+
+    tree = {
+        'index': 0, 
+        'right': {
+            'index': 6, 
+            'right': {
+                'index': 0, 
+                'right': {
+                    'index': 3, 
+                    'right': 0.0214471, # terminal node 37
+                    'value': 1130.84399, 
+                    'left': {
+                        'index': 2, 
+                        'right': 0.0408473, # terminal node 36
+                        'value': 5.29967, 
+                        'left': 0.0818293 # terminal node 35
+                    }
+                }, 
+                'value': 6.08065, 
+                'left': {
+                    'index': 1, 
+                    'right': {
+                        'index': 0, 
+                        'right': 0.175359, # terminal node 34
+                        'value': 5.89440, 
+                        'left': 0.0575384 # terminal node 33
+                    }, 
+                    'value': 624.57501, 
+                    'left': {
+                        'index': 1, 
+                        'right': {
+                            'index': 0, 
+                            'right': 0.202195, # terminal node 32
+                            'value': 4.63185, # CONFERIR AQUI <<<<========================================================
+                            'left': 0.0978987 # terminal node 31
+                        }, 
+                        'value': 506.88501, 
+                        'left': 0.0315917 # terminal node 30
+                    }
+                }
+            }, 
+            'value': 55.50000, 
+            'left': {
+                'index': 3, 
+                'right': {
+                    'index': 1, 
+                    'right': 0.0473021, # terminal node 29
+                    'value': 736.80499, 
+                    'left': {
+                        'index': 5, 
+                        'right': {
+                            'index': 3, 
+                            'right': {
+                                'index': 0, 
+                                'right': 0.586747, # terminal node 28
+                                'value': 24.23654, 
+                                'left': 0.0750805 # terminal node 27
+                            }, 
+                            'value': 1371.60449, 
+                            'left': 0.125505 # terminal node 26
+                        }, 
+                        'value': 6, 
+                        'left': 0.0639006 # terminal node 25
+                    }
+                }, 
+                'value': 1169.50452, 
+                'left': {
+                    'index': 7, 
+                    'right': 0.0699455, # terminal node 24
+                    'value': 25.03165, 
+                    'left': 0.185039 # terminal node 23
+                }
+            }
+        }, 
+        'value': 3.63463, 
+        'left': {
+            'index': 5, 
+            'right': {
+                'index': 3, 
+                'right': {
+                    'index': 7, 
+                    'right': {
+                        'index': 0, 
+                        'right': {
+                            'index': 0, 
+                            'right': 0.571113, # terminal node 22
+                            'value': 2.50642, 
+                            'left': 0.239003 # terminal node 21
+                        }, 
+                        'value': 2.35704, 
+                        'left': {
+                            'index': 7, 
+                            'right': 0.338749, # terminal node 20
+                            'value': 24.71705, 
+                            'left': 0.0901171 # terminal node 19
+                        }
+                    }, 
+                    'value': 24.68275, 
+                    'left': {
+                        'index': 1, 
+                        'right': {
+                            'index': 0, 
+                            'right': 0.52072, # terminal node 18
+                            'value': 2.35704, 
+                            'left': 0.0606344 # terminal node 17
+                        }, 
+                        'value': 859.72498, 
+                        'left': {
+                            'index': 1, 
+                            'right': 0.748276, # terminal node 16
+                            'value': 684.70502, 
+                            'left': 0.281141 # terminal node 15
+                        }
+                    }
+                }, 
+                'value': 1350.50757, 
+                'left': {
+                    'index': 1, 
+                    'right': 0.250526, # terminal node 14
+                    'value': 859.72498, 
+                    'left': {
+                        'index': 7, 
+                        'right': 0.150805, # terminal node 13
+                        'value': 24.75305, 
+                        'left': 0.0372407 # terminal node 12
+                    }
+                }
+            }, 
+            'value': 8, 
+            'left': {
+                'index': 3, 
+                'right': {
+                    'index': 0,
+                    'right': 0.139411, # terminal node 11
+                    'value': 3.26368, 
+                    'left': {
+                        'index': 4,
+                        'right': {
+                            'index': 6,
+                            'right': 0.0499697, # terminal node 10
+                            'value': 44.50000, 
+                            'left': {
+                                'index': 7,
+                                'right': 0.131192, # terminal node 09
+                                'value': 24.79185, 
+                                'left': 0.0168376 # terminal node 08
+                            }
+                        },
+                        'value': 54.5, 
+                        'left': 0.109868 # terminal node 07
+                    }
+                }, 
+                'value': 1109.07446, 
+                'left': {
+                    'index': 1, 
+                    'right': {
+                        'index': 1, 
+                        'right': {
+                            'index': 1, 
+                            'right': 0.175612, # terminal node 06
+                            'value': 864.86499, 
+                            'left': 0.434419 # terminal node 05
+                        }, 
+                        'value': 841.71002, 
+                        'left': 0.110923 # terminal node 04
+                    }, 
+                    'value': 556.09497, 
+                    'left': {
+                        'index': 3, 
+                        'right': {
+                            'index': 3, 
+                            'right': 0.18456, # terminal node 03
+                            'value': 1090.74597, 
+                            'left': 0.330305 # terminal node 02
+                        }, 
+                        'value': 1034.39551, 
+                        'left': 0.0835317 # terminal node 01
+                    }
+                }
+            }
+        }
+    }
+    for index, row in enumerate(dataset):
+        prediction = predict(tree, row)
+        #if row[-1] != prediction:
+        #	print('Rule=%d - Expected=%f, Got=%f' % (index+1, row[-1], prediction))
+        print('Rule=%d - Expected=%f, Got=%f' % (index+1, row[-1], prediction))
+    '''
     while True:
         print 'Informa a Densidade Demografica: '
         dens_demo = float(raw_input('> '))
@@ -23,10 +265,10 @@ if __name__ == '__main__':
         print '2 - ÁREAS URBANAS'
         print '3 - CURSO D’ÁGUA'
         print '4 - FLORESTA NATURAL'
-        print '5 - MANGUEZAIS'
+        print '5 - SOLO EXPOSTO'
         print '6 - PASTAGEM'
         print '7 - SILVICULTURA'
-        print '8 - SOLO EXPOSTO'
+        print '8 - MANGUEZAIS'
         print '9 - ÁREAS ALAGADAS'
         print '10 - RESTINGA'
         uso_terra = int(raw_input('> '))
@@ -36,114 +278,4 @@ if __name__ == '__main__':
         temp = float(raw_input('> '))
         if not dens_demo or not renda or not SLOPE or not prec or not VCF or not uso_terra or not DEM or not temp:
             break
-        #Rules for terminal node 1
-        if dens_demo <= 3.63463 and uso_terra <= 8 and prec <= 1034.39551 and renda <= 556.09497:
-            print "terminalNode = -1 \nmean = 0.0835317"
-        #Rules for terminal node 2
-        if dens_demo <= 3.63463 and uso_terra <= 8 and prec > 1034.39551 and prec <= 1090.74597 and renda <= 556.09497:
-            print "terminalNode = -2 \nmean = 0.330305"
-        #Rules for terminal node 3
-        if dens_demo <= 3.63463 and uso_terra <= 8 and prec > 1090.74597 and prec <= 1109.07446 and renda <= 556.09497:
-            print "terminalNode = -3 \nmean = 0.18456"
-        #Rules for terminal node 4
-        if dens_demo <= 3.63463 and uso_terra <= 8 and prec <= 1109.07446 and renda > 556.09497 and renda <= 841.71002:
-            print "terminalNode = -4 \nmean = 0.110923"
-        #Rules for terminal node 5
-        if dens_demo <= 3.63463 and uso_terra <= 8 and prec <= 1109.07446 and renda > 841.71002 and renda <= 864.86499:
-            print "terminalNode = -5 \nmean = 0.434419"
-        #Rules for terminal node 6
-        if dens_demo <= 3.63463 and uso_terra <= 8 and prec <= 1109.07446 and renda > 864.86499:
-            print "terminalNode = -6 \nmean = 0.175612"
-        #Rules for terminal node 7
-        if dens_demo <= 3.26368 and uso_terra <= 8 and prec > 1109.07446 and VCF <= 54.5:
-            print "terminalNode = -7 \nmean = 0.109868"
-        #Rules for terminal node 8
-        if dens_demo <= 3.26368 and uso_terra <= 8 and prec > 1109.07446 and VCF > 54.5 and DEM <= 44.50000 and temp <= 24.79185:
-            print "terminalNode = -8 \n mean = 0.0168376"
-        #Rules for terminal node 9
-        if dens_demo <= 3.26368 and uso_terra <= 8 and prec > 1109.07446 and VCF > 54.5 and DEM <= 44.50000 and temp > 24.79185:
-            print "terminalNode = -9 \nmean = 0.131192"
-        #Rules for terminal node 10
-        if dens_demo <= 3.26368 and uso_terra <= 8 and prec > 1109.07446 and VCF > 54.5 and DEM > 44.50000:
-            print "terminalNode = -10 \nmean = 0.0499697"
-        #Rules for terminal node 11
-        if dens_demo > 3.26368 and dens_demo <= 3.63463 and uso_terra <= 8 and prec > 1109.07446:
-            print "terminalNode = -11 \nmean = 0.139411"
-        #Rules for terminal node 12
-        if dens_demo <= 3.63463 and uso_terra > 8 and prec <= 1350.50757 and renda <= 859.72498 and temp <= 24.75305:
-            print "terminalNode = -12 \nmean = 0.0372407"
-        #Rules for terminal node 13
-        if dens_demo <= 3.63463 and uso_terra > 8 and prec <= 1350.50757 and renda <= 859.72498 and temp > 24.75305:
-            print "terminalNode = -13 \nmean = 0.150805"
-        #Rules for terminal node 14
-        if dens_demo <= 3.63463 and uso_terra > 8 and prec <= 1350.50757 and renda > 859.72498:
-            print "terminalNode = -14 \nmean = 0.250526"
-        #Rules for terminal node 15
-        if dens_demo <= 3.63463 and uso_terra > 8 and prec > 1350.50757 and renda <= 684.70502 and temp <= 24.68275:
-            print "terminalNode = -15 \nmean = 0.281141"
-        #Rules for terminal node 16
-        if dens_demo <= 3.63463 and uso_terra > 8 and prec > 1350.50757 and renda > 684.70502 and renda <= 854.57001 and temp <= 24.68275:
-            print "terminalNode = -16 \nmean = 0.748276"
-        #Rules for terminal node 17
-        if dens_demo <= 2.35704 and uso_terra > 8 and prec > 1350.50757 and renda > 854.57001 and temp <= 24.68275:
-            print "terminalNode = -17 \nmean = 0.0606344"
-        #Rules for terminal node 18
-        if dens_demo > 2.35704 and dens_demo <= 3.63463 and uso_terra > 8 and prec > 1350.50757 and renda > 854.57001 and temp <= 24.68275:
-            print "terminalNode = -18 \nmean = 0.52072"
-        #Rules for terminal node 19
-        if dens_demo <= 2.35704 and uso_terra > 8 and prec > 1350.50757 and temp > 24.68275 and temp <= 24.71705:
-            print "terminalNode = -19 \nmean = 0.0901171"
-        #Rules for terminal node 20
-        if dens_demo <= 2.35704 and uso_terra > 8 and prec > 1350.50757 and temp > 24.71705:
-            print "terminalNode = -20 \nmean = 0.338749"
-        #Rules for terminal node 21
-        if dens_demo > 2.35704 and dens_demo <= 2.50642 and uso_terra > 8 and prec > 1350.50757 and temp > 24.68275:
-            print "terminalNode = -21 \nmean = 0.239003"
-        #Rules for terminal node 22
-        if dens_demo > 2.50642 and dens_demo <= 3.63463 and uso_terra > 8 and prec > 1350.50757 and temp > 24.68275:
-            print "terminalNode = -22 \nmean = 0.571113"
-        #Rules for terminal node 23
-        if dens_demo > 3.63463 and prec <= 1169.50452 and temp <= 25.03165 and DEM <= 55.50000:
-            print "terminalNode = -23 \nmean = 0.185039"
-        #Rules for terminal node 24
-        if dens_demo > 3.63463 and prec <= 1169.50452 and temp > 25.03165 and DEM <= 55.50000:
-            print "terminalNode = -24 \nmean = 0.0699455"
-        #Rules for terminal node 25
-        if dens_demo > 3.63463 and (uso_terra <= 4 or uso_terra == 6 or uso_terra == 8) and prec > 1169.50452 and renda <= 736.80499 and DEM <= 55.50000:
-            print "terminalNode = -25 \nmean = 0.0639006"
-        #Rules for terminal node 26
-        if dens_demo > 3.63463 and (uso_terra == 5 or uso_terra == 7 or uso_terra > 8) and prec > 1169.50452 and prec <= 1371.60449 and renda <= 736.80499 and DEM <= 55.50000:
-            print "terminalNode = -26 \nmean = 0.125505"
-        #Rules for terminal node 27
-        if dens_demo > 3.63463 and dens_demo <= 24.23654 and (uso_terra == 5 or uso_terra == 7 or uso_terra > 8) and prec > 1371.60449 and renda <= 736.80499 and DEM <= 55.50000:
-            print "terminalNode = -27 \nmean = 0.0750805"
-        #Rules for terminal node 28
-        if dens_demo > 24.23654 and (uso_terra == 5 or uso_terra == 7 or uso_terra > 8) and prec > 1371.60449 and renda <= 736.80499 and DEM <= 55.50000:
-            print "terminalNode = -28 \nmean = 0.586747"
-        #Rules for terminal node 29
-        if dens_demo > 3.63463 and prec > 1169.50452 and renda > 736.80499 and DEM <= 55.50000:
-            print "terminalNode = -29 \nmean = 0.0473021"
-        #Rules for terminal node 30
-        if dens_demo > 3.63463 and dens_demo <= 6.08065 and renda <= 506.88501 and DEM > 55.50000:
-            print "terminalNode = -30 \nmean = 0.0315917"
-        #Rules for terminal node 31
-        if dens_demo > 3.63463 and dens_demo <= 4.63185 and renda > 506.88501 and renda <= 624.57501 and DEM > 55.50000:
-            print "terminalNode = -31 \nmean = 0.0978987"
-        #Rules for terminal node 32
-        if dens_demo > 4.63185 and dens_demo <= 6.08065 and renda > 506.88501 and renda <= 624.57501 and DEM > 55.50000:
-            print "terminalNode = -32 \nmean = 0.202195"
-        #Rules for terminal node 33
-        if dens_demo > 3.63463 and dens_demo <= 5.89440 and renda > 624.57501 and DEM > 55.50000:
-            print "terminalNode = -33 \nmean = 0.0575384"
-        #Rules for terminal node 34
-        if dens_demo > 5.89440 and dens_demo <= 6.08065 and renda > 624.57501 and DEM > 55.50000:
-            print "terminalNode = -34 \nmean = 0.175359"
-        #Rules for terminal node 35
-        if dens_demo > 6.08065 and prec <= 1130.84399 and DEM > 55.50000 and SLOPE <= 5.29967:
-            print "terminalNode = -35 \nmean = 0.0818293"
-        #Rules for terminal node 36
-        if dens_demo > 6.08065 and prec <= 1130.84399 and DEM > 55.50000 and SLOPE > 5.29967:
-            print "terminalNode = -36 \nmean = 0.0408473"
-        #Rules for terminal node 37
-        if dens_demo > 6.08065 and prec > 1130.84399 and DEM > 55.50000:
-            print "terminalNode = -37 \nmean = 0.0214471"
+        '''
